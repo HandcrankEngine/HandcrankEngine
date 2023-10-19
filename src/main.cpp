ba7d98692e3943d42bd785f88729e4bdc00e3885
Scott Doxey <neogeek0101@gmail.com>
2023-10-19 03:14:54 -0400

    Updated demo.

--- src/main.cpp
+++ src/main.cpp
@@ -12,31 +12,107 @@ using namespace Handcrank;
 
 auto game = new Game();
 
-SDL_Color colors[3] = {{255, 0, 0, 255}, {0, 255, 0, 255}, {0, 0, 255, 255}};
+auto font = SDL_TTF_Utilities::LoadFontRW(
+    fonts_Roboto_Roboto_Regular_ttf, fonts_Roboto_Roboto_Regular_ttf_len, 30);
 
-const int colorIndexMax = sizeof(colors) / sizeof(colors[0]);
-
-int colorIndex = 0;
-
-auto selectNextColor() -> SDL_Color
+class Logo : public RenderObject
 {
-    colorIndex++;
+    SDL_Color colors[3] = {
+        {255, 0, 0, 255}, {0, 255, 0, 255}, {0, 0, 255, 255}};
 
-    if (colorIndex > colorIndexMax - 1)
+    int colorIndex = 0;
+
+    int colorIndexMax = 3;
+
+    auto selectNextColor() -> SDL_Color
     {
-        colorIndex = 0;
+        colorIndex++;
+
+        if (colorIndex > colorIndexMax - 1)
+        {
+            colorIndex = 0;
+        }
+
+        return colors[colorIndex];
     }
 
-    return colors[colorIndex];
-}
+    int xDirection = 1;
+    int yDirection = 1;
+
+    int movementSpeed = 500;
+
+    void Start() override
+    {
+        SetScale(1.5);
+
+        SetRect(0, 0, 200, 200);
+
+        auto cube = std::make_unique<RectRenderObject>();
+
+        cube->SetColor(colors[colorIndex]);
+
+        cube->SetRect(0, 0, GetRect()->w, GetRect()->h);
+
+        AddChildObject(std::move(cube));
+
+        auto text = std::make_unique<TextRenderObject>();
+
+        text->SetFont(font);
+
+        text->SetRect(0, 0, GetRect()->w, GetRect()->h);
+
+        text->SetWrappedText("Handcrank\nEngine");
+
+        AddChildObject(std::move(text));
+    }
+
+    void Update(double deltaTime) override
+    {
+        if (!game->HasFocus())
+        {
+            return;
+        }
+
+        auto transformedRect = GetTransformedRect();
+
+        auto rect = GetRect();
+
+        auto cube = GetChildByType<RectRenderObject>();
+
+        auto maxX = game->GetWidth() - transformedRect->w;
+        auto maxY = game->GetHeight() - transformedRect->h;
+
+        rect->x += movementSpeed * xDirection * deltaTime;
+        rect->y += movementSpeed * yDirection * deltaTime;
+
+        if (rect->x > maxX || rect->x < 0)
+        {
+            xDirection = -xDirection;
+
+            if (cube != nullptr)
+            {
+                cube->SetColor(selectNextColor());
+            }
+        }
+
+        if (rect->y > maxY || rect->y < 0)
+        {
+            yDirection = -yDirection;
+
+            if (cube != nullptr)
+            {
+                cube->SetColor(selectNextColor());
+            }
+        }
+
+        rect->x = std::clamp<float>(rect->x, 0, maxX);
+        rect->y = std::clamp<float>(rect->y, 0, maxY);
+    }
+};
 
 auto main() -> int
 {
     game->SetTitle("Handcrank Engine");
-
-    auto font =
-        SDL_TTF_Utilities::LoadFontRW(fonts_Roboto_Roboto_Regular_ttf,
-                                      fonts_Roboto_Roboto_Regular_ttf_len, 30);
 
     auto framerateLabel = std::make_unique<TextRenderObject>();
 
@@ -54,78 +130,7 @@ auto main() -> int
 
     game->AddChildObject(std::move(framerateLabel));
 
-    auto logo = std::make_unique<RenderObject>();
-
-    logo->SetScale(1.5);
-
-    logo->SetRect(0, 0, 200, 200);
-
-    auto cube = std::make_unique<RectRenderObject>();
-
-    auto text = std::make_unique<TextRenderObject>();
-
-    text->SetFont(font);
-
-    text->SetRect(0, 0, logo->GetRect()->w, logo->GetRect()->h);
-
-    text->SetWrappedText("Handcrank\nEngine");
-
-    cube->SetColor(colors[colorIndex]);
-
-    cube->SetRect(0, 0, logo->GetRect()->w, logo->GetRect()->h);
-
-    logo->AddChildObject(std::move(cube));
-    logo->AddChildObject(std::move(text));
-
-    int xDirection = 1;
-    int yDirection = 1;
-
-    int movementSpeed = 500;
-
-    logo->SetUpdate(
-        [&xDirection, &yDirection, movementSpeed](RenderObject *ref,
-                                                  double deltaTime)
-        {
-            if (!ref->game->HasFocus())
-            {
-                return;
-            }
-
-            auto transformedRect = ref->GetTransformedRect();
-
-            auto rect = ref->GetRect();
-
-            auto cube = ref->GetChildByType<RectRenderObject>();
-
-            auto maxX = ref->game->GetWidth() - transformedRect->w;
-            auto maxY = ref->game->GetHeight() - transformedRect->h;
-
-            rect->x += movementSpeed * xDirection * deltaTime;
-            rect->y += movementSpeed * yDirection * deltaTime;
-
-            if (rect->x > maxX || rect->x < 0)
-            {
-                xDirection = -xDirection;
-
-                if (cube != nullptr)
-                {
-                    cube->SetColor(selectNextColor());
-                }
-            }
-
-            if (rect->y > maxY || rect->y < 0)
-            {
-                yDirection = -yDirection;
-
-                if (cube != nullptr)
-                {
-                    cube->SetColor(selectNextColor());
-                }
-            }
-
-            rect->x = std::clamp<float>(rect->x, 0, maxX);
-            rect->y = std::clamp<float>(rect->y, 0, maxY);
-        });
+    auto logo = std::make_unique<Logo>();
 
     game->AddChildObject(std::move(logo));
 
