a3e0f7b86bedeee292e2000a81f864e6c47d7dc9
Scott Doxey <neogeek0101@gmail.com>
2023-10-27 12:32:43 -0400

    Added SDL image and ttf to test script.

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -18,12 +18,38 @@ else
     exit 1
 fi
 
+SDL2_IMAGE_PATH=""
+
+if [ -d "/opt/homebrew/Cellar/sdl2_image/2.6.3_2" ]; then
+    SDL2_IMAGE_PATH="/opt/homebrew/Cellar/sdl2_image/2.6.3_2"
+elif [ -d "/tmp/.sdl/SDL2_image-2.6.3" ]; then
+    SDL2_IMAGE_PATH="/tmp/.sdl/SDL2_image-2.6.3"
+else
+    echo "SDL2_image not found!"
+
+    exit 1
+fi
+
+SDL2_TTF_PATH=""
+
+if [ -d "/opt/homebrew/Cellar/sdl2_ttf/2.20.2" ]; then
+    SDL2_TTF_PATH="/opt/homebrew/Cellar/sdl2_ttf/2.20.2"
+elif [ -d "/tmp/.sdl/SDL2_ttf-2.20.2" ]; then
+    SDL2_TTF_PATH="/tmp/.sdl/SDL2_ttf-2.20.2"
+else
+    echo "SDL2_ttf not found!"
+
+    exit 1
+fi
+
 find ./tests -type f -name "*.cpp" | while read -r FILE; do
 
     printf "Running tests in %s ... " "${FILE}"
 
     g++ -std=c++17 -o test "${FILE}" -Iincludes \
         -I"${SDL2_PATH}/include/SDL2" -L"${SDL2_PATH}/lib" \
+        -I"${SDL2_IMAGE_PATH}/include/SDL2" -L"${SDL2_IMAGE_PATH}/lib" \
+        -I"${SDL2_TTF_PATH}/include/SDL2" -L"${SDL2_TTF_PATH}/lib" \
         -lSDL2
 
     if ./test; then
