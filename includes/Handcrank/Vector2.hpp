1e912852260959f8bee0aeabb803126f3dd9c145
Scott Doxey <neogeek0101@gmail.com>
2023-10-27 00:39:26 -0400

    Simplified constructors.

--- includes/Handcrank/Vector2.hpp
+++ includes/Handcrank/Vector2.hpp
@@ -8,7 +8,7 @@ struct Vector2
     float x;
     float y;
 
-    Vector2() : x(0.0f), y(0.0f) {}
+    Vector2() : x(0), y(0) {}
     Vector2(float x, float y) : x(x), y(y) {}
 
     Vector2 operator+(const Vector2 &other) const
--- includes/Handcrank/Vector3.hpp
+++ includes/Handcrank/Vector3.hpp
@@ -9,7 +9,7 @@ struct Vector3
     float y;
     float z;
 
-    Vector3() : x(0.0f), y(0.0f), z(0.0f) {}
+    Vector3() : x(0), y(0), z(0) {}
     Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
 
     Vector3 operator+(const Vector3 &other) const
