8727041d9089c786a81d072a16b808fb1a213457
Scott Doxey <neogeek0101@gmail.com>
2023-10-29 14:06:26 -0400

    Added box2d demo.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include "Handcrank/Handcrank.hpp"
+#include <box2d/box2d.h>
 
-#include "../examples/fps/FPS.hpp"
-#include "../examples/logo-screensaver/LogoScreenSaver.hpp"
+#include "Handcrank/Handcrank.hpp"
+#include "Handcrank/RectRenderObject.hpp"
 
 using namespace Handcrank;
 
@@ -11,9 +11,34 @@ auto main() -> int
 {
     game->SetTitle("Handcrank Engine");
 
-    game->AddChildObject(std::move(std::make_unique<FPS>()));
+    b2Vec2 gravity(0.0f, -9.8f);
+    b2World world(gravity);
 
-    game->AddChildObject(std::move(std::make_unique<LogoScreenSaver>()));
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    b2Body *body = world.CreateBody(&bodyDef);
+
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(50.0f, 50.0f);
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBox;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 0.3f;
+    body->CreateFixture(&fixtureDef);
+
+    auto box = std::make_unique<RectRenderObject>();
+
+    box->SetFillColor(255, 0, 0, 255);
+
+    box->SetFixedUpdate(
+        [&world, &body](RenderObject *ref, double deltaTime)
+        {
+            ref->SetRect(body->GetPosition().x, -body->GetPosition().y);
+
+            world.Step(1.0f / 60.0f, 6, 2);
+        });
+
+    game->AddChildObject(std::move(box));
 
     return game->Run();
 }
