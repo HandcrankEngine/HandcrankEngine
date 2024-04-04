20a8138b486542a942a4476360c298559fde060b
Scott Doxey <neogeek0101@gmail.com>
2024-04-04 13:29:00 -0400

    Updated SDL.

--- bin/find-sdl.sh
+++ bin/find-sdl.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-if [ -d "/opt/homebrew/Cellar/sdl2/2.28.5" ]; then
-    export SDL2_PATH="/opt/homebrew/Cellar/sdl2/2.28.5"
-elif [ -d "/tmp/.sdl/SDL2-2.28.5" ]; then
-    export SDL2_PATH="/tmp/.sdl/SDL2-2.28.5"
+if [ -d "/opt/homebrew/Cellar/sdl2/2.30.2" ]; then
+    export SDL2_PATH="/opt/homebrew/Cellar/sdl2/2.30.2"
+elif [ -d "/tmp/.sdl/SDL2-2.30.2" ]; then
+    export SDL2_PATH="/tmp/.sdl/SDL2-2.30.2"
 else
     echo "SDL2 not found!"
 
     exit 1
 fi
 
-if [ -d "/opt/homebrew/Cellar/sdl2_image/2.6.3_2" ]; then
-    export SDL2_IMAGE_PATH="/opt/homebrew/Cellar/sdl2_image/2.6.3_2"
-elif [ -d "/tmp/.sdl/SDL2_image-2.6.3" ]; then
-    export SDL2_IMAGE_PATH="/tmp/.sdl/SDL2_image-2.6.3"
+if [ -d "/opt/homebrew/Cellar/sdl2_image/2.8.2_1" ]; then
+    export SDL2_IMAGE_PATH="/opt/homebrew/Cellar/sdl2_image/2.8.2_1"
+elif [ -d "/tmp/.sdl/SDL2_image-2.8.2" ]; then
+    export SDL2_IMAGE_PATH="/tmp/.sdl/SDL2_image-2.8.2"
 else
     echo "SDL2_image not found!"
 
     exit 1
 fi
 
-if [ -d "/opt/homebrew/Cellar/sdl2_ttf/2.20.2" ]; then
-    export SDL2_TTF_PATH="/opt/homebrew/Cellar/sdl2_ttf/2.20.2"
-elif [ -d "/tmp/.sdl/SDL2_ttf-2.20.2" ]; then
-    export SDL2_TTF_PATH="/tmp/.sdl/SDL2_ttf-2.20.2"
+if [ -d "/opt/homebrew/Cellar/sdl2_ttf/2.22.0" ]; then
+    export SDL2_TTF_PATH="/opt/homebrew/Cellar/sdl2_ttf/2.22.0"
+elif [ -d "/tmp/.sdl/SDL2_ttf-2.22.0" ]; then
+    export SDL2_TTF_PATH="/tmp/.sdl/SDL2_ttf-2.22.0"
 else
     echo "SDL2_ttf not found!"
 
