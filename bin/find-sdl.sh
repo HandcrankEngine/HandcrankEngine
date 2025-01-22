f855fbd73493f898b0aa87522e128123034f22e1
Scott Doxey <neogeek0101@gmail.com>
2025-01-22 00:32:31 -0500

    Simplified the find sdl script.

--- bin/find-sdl.sh
+++ bin/find-sdl.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
-if [ -d "/opt/homebrew/Cellar/sdl2/2.30.10" ]; then
-    export SDL2_PATH="/opt/homebrew/Cellar/sdl2/2.30.10"
-elif [ -d "/tmp/.sdl/SDL2-2.30.10" ]; then
-    export SDL2_PATH="/tmp/.sdl/SDL2-2.30.10"
-else
-    echo "SDL2 not found!"
+[[ ! -d "${SDL2_PATH}" && -d "/opt/homebrew/Cellar/sdl2" ]] &&
+    export SDL2_PATH=$(find /opt/homebrew/Cellar/sdl2 -name "2.*" -type d | head -n 1)
+[[ ! -d "${SDL2_PATH}" && -d "/tmp/.sdl/" ]] &&
+    export SDL2_PATH=$(find /tmp/.sdl/ -name "SDL2-2.*" -type d | head -n 1)
 
-    exit 1
-fi
+[ ! -d "${SDL2_PATH}" ] && echo "SDL2 not found!" && exit 1
 
-if [ -d "/opt/homebrew/Cellar/sdl2_image/2.8.3" ]; then
-    export SDL2_IMAGE_PATH="/opt/homebrew/Cellar/sdl2_image/2.8.3"
-elif [ -d "/tmp/.sdl/SDL2_image-2.8.3" ]; then
-    export SDL2_IMAGE_PATH="/tmp/.sdl/SDL2_image-2.8.3"
-else
-    echo "SDL2_image not found!"
+[[ ! -d "${SDL2_IMAGE_PATH}" && -d "/opt/homebrew/Cellar/sdl2_image" ]] &&
+    export SDL2_IMAGE_PATH=$(find /opt/homebrew/Cellar/sdl2_image -name "2.*" -type d | head -n 1)
+[[ ! -d "${SDL2_IMAGE_PATH}" && -d "/tmp/.sdl/" ]] &&
+    export SDL2_IMAGE_PATH=$(find /tmp/.sdl/ -name "SDL2_image-2.*" -type d | head -n 1)
 
-    exit 1
-fi
+[ ! -d "${SDL2_IMAGE_PATH}" ] && echo "SDL2_image not found!" && exit 1
 
-if [ -d "/opt/homebrew/Cellar/sdl2_ttf/2.22.0" ]; then
-    export SDL2_TTF_PATH="/opt/homebrew/Cellar/sdl2_ttf/2.22.0"
-elif [ -d "/tmp/.sdl/SDL2_ttf-2.22.0" ]; then
-    export SDL2_TTF_PATH="/tmp/.sdl/SDL2_ttf-2.22.0"
-else
-    echo "SDL2_ttf not found!"
+[[ ! -d "${SDL2_TTF_PATH}" && -d "/opt/homebrew/Cellar/sdl2_ttf" ]] &&
+    export SDL2_TTF_PATH=$(find /opt/homebrew/Cellar/sdl2_ttf -name "2.*" -type d | head -n 1)
+[[ ! -d "${SDL2_TTF_PATH}" && -d "/tmp/.sdl/" ]] &&
+    export SDL2_TTF_PATH=$(find /tmp/.sdl/ -name "SDL2_ttf-2.*" -type d | head -n 1)
 
-    exit 1
-fi
+[ ! -d "${SDL2_TTF_PATH}" ] && echo "SDL2_ttf not found!" && exit 1
