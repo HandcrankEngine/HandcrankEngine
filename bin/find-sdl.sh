962c66ee8e532d338ab2beee11eeb06821ae6bee
Scott Doxey <neogeek0101@gmail.com>
2025-02-04 13:18:38 -0500

    Output results of find-sdl script.

--- bin/find-sdl.sh
+++ bin/find-sdl.sh
@@ -7,6 +7,8 @@
 
 [ ! -d "${SDL2_PATH}" ] && echo "SDL2 not found!" && exit 1
 
+echo "SDL2 found at ${SDL2_PATH}"
+
 [[ ! -d "${SDL2_IMAGE_PATH}" && -d "/opt/homebrew/Cellar/sdl2_image" ]] &&
     export SDL2_IMAGE_PATH=$(find /opt/homebrew/Cellar/sdl2_image -name "2.*" -type d | head -n 1)
 [[ ! -d "${SDL2_IMAGE_PATH}" && -d "/tmp/.sdl/" ]] &&
@@ -14,9 +16,13 @@
 
 [ ! -d "${SDL2_IMAGE_PATH}" ] && echo "SDL2_image not found!" && exit 1
 
+echo "SDL2_image found at ${SDL2_IMAGE_PATH}"
+
 [[ ! -d "${SDL2_TTF_PATH}" && -d "/opt/homebrew/Cellar/sdl2_ttf" ]] &&
     export SDL2_TTF_PATH=$(find /opt/homebrew/Cellar/sdl2_ttf -name "2.*" -type d | head -n 1)
 [[ ! -d "${SDL2_TTF_PATH}" && -d "/tmp/.sdl/" ]] &&
     export SDL2_TTF_PATH=$(find /tmp/.sdl/ -name "SDL2_ttf-2.*" -type d | head -n 1)
 
 [ ! -d "${SDL2_TTF_PATH}" ] && echo "SDL2_ttf not found!" && exit 1
+
+echo "SDL2_ttf found at ${SDL2_TTF_PATH}"
