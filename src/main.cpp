a183dbb0f40ce1c04bd29ccec387c73035d11595
Scott Doxey <neogeek0101@gmail.com>
2025-03-06 18:50:44 -0500

    Updated demo.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "Handcrank/Handcrank.hpp"
 
+#include "../examples/box/Box.hpp"
 #include "../examples/fps/FPS.hpp"
 #include "../examples/logo-screensaver/LogoScreenSaver.hpp"
 
@@ -7,12 +8,25 @@ using namespace Handcrank;
 
 auto game = new Game();
 
+class InputManager : public RenderObject
+{
+  public:
+    void Update(const double deltaTime) override
+    {
+        if (game->keyState[SDLK_ESCAPE])
+        {
+            game->Quit();
+        }
+    }
+};
+
 auto main() -> int
 {
     game->SetTitle("Handcrank Engine");
 
+    game->AddChildObject(std::move(std::make_unique<Box>()));
     game->AddChildObject(std::move(std::make_unique<FPS>()));
-
+    game->AddChildObject(std::move(std::make_unique<InputManager>()));
     game->AddChildObject(std::move(std::make_unique<LogoScreenSaver>()));
 
     return game->Run();
