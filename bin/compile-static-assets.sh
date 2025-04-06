08868c0786200eb7e547ba30a302e599e4098874
Scott Doxey <neogeek0101@gmail.com>
2025-04-05 22:30:36 -0400

    Fixed invalid folder check in bash script.

--- bin/compile-static-assets.sh
+++ bin/compile-static-assets.sh
@@ -9,6 +9,6 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
     cd ..
 
     [ -d "fonts" ] && find fonts -type f -name "*.ttf" -exec sh -c 'echo "#pragma once\n" > "${0%.ttf}.h" && xxd -i "$0" >> "${0%.ttf}.h"' {} \;
-    [ -d "fonts" ] && find images -type f -name "*.png" -exec sh -c 'echo "#pragma once\n" > "${0%.png}.h" && xxd -i "$0" >> "${0%.png}.h"' {} \;
+    [ -d "images" ] && find images -type f -name "*.png" -exec sh -c 'echo "#pragma once\n" > "${0%.png}.h" && xxd -i "$0" >> "${0%.png}.h"' {} \;
 
 )
