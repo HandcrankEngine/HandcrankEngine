5814639779023a77eb39c089693b8d6577a45419
Scott Doxey <neogeek0101@gmail.com>
2025-05-30 15:04:23 -0400

    Added flag to reduce web export size.
    Added gzip to web export.

--- bin/build-web.sh
+++ bin/build-web.sh
@@ -4,10 +4,14 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 (
 
+    rm -rf build/web
+
     mkdir -p build/web
 
-    emcc -std=c++17 -o build/web/index.html src/*.cpp -Ifonts -Iimages -Iinclude -Iexamples -Isrc \
+    emcc -std=c++17 -Os -o build/web/index.html src/*.cpp -Ifonts -Iimages -Iinclude -Iexamples -Isrc \
         -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 \
         --shell-file templates/web-minimal.html
+
+    gzip -k build/web/index.wasm
 
 )
