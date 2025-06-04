d21258f63d815c3657133f7395f3e322b65cf748
Scott Doxey <neogeek0101@gmail.com>
2025-06-03 23:30:51 -0400

    Changed paths to check for SDL in.

--- bin/find-sdl.sh
+++ bin/find-sdl.sh
@@ -3,7 +3,7 @@
 [[ ! -d "${SDL2_PATH}" && -d "/opt/homebrew/Cellar/sdl2" ]] &&
     export SDL2_PATH=$(find /opt/homebrew/Cellar/sdl2 -name "2.*" -type d | head -n 1)
 [[ ! -d "${SDL2_PATH}" && -d "/tmp/.sdl/" ]] &&
-    export SDL2_PATH=$(find /tmp/.sdl/ -name "SDL2-2.*" -type d | head -n 1)
+    export SDL2_PATH=$(find /tmp/.sdl/ -name "SDL-*" -type d | head -n 1)
 
 [ ! -d "${SDL2_PATH}" ] && echo "SDL2 not found!" && exit 1
 
@@ -12,7 +12,7 @@ echo "SDL2 found at ${SDL2_PATH}"
 [[ ! -d "${SDL2_IMAGE_PATH}" && -d "/opt/homebrew/Cellar/sdl2_image" ]] &&
     export SDL2_IMAGE_PATH=$(find /opt/homebrew/Cellar/sdl2_image -name "2.*" -type d | head -n 1)
 [[ ! -d "${SDL2_IMAGE_PATH}" && -d "/tmp/.sdl/" ]] &&
-    export SDL2_IMAGE_PATH=$(find /tmp/.sdl/ -name "SDL2_image-2.*" -type d | head -n 1)
+    export SDL2_IMAGE_PATH=$(find /tmp/.sdl/ -name "SDL_image-*" -type d | head -n 1)
 
 [ ! -d "${SDL2_IMAGE_PATH}" ] && echo "SDL2_image not found!" && exit 1
 
@@ -21,7 +21,7 @@ echo "SDL2_image found at ${SDL2_IMAGE_PATH}"
 [[ ! -d "${SDL2_TTF_PATH}" && -d "/opt/homebrew/Cellar/sdl2_ttf" ]] &&
     export SDL2_TTF_PATH=$(find /opt/homebrew/Cellar/sdl2_ttf -name "2.*" -type d | head -n 1)
 [[ ! -d "${SDL2_TTF_PATH}" && -d "/tmp/.sdl/" ]] &&
-    export SDL2_TTF_PATH=$(find /tmp/.sdl/ -name "SDL2_ttf-2.*" -type d | head -n 1)
+    export SDL2_TTF_PATH=$(find /tmp/.sdl/ -name "SDL_ttf-*" -type d | head -n 1)
 
 [ ! -d "${SDL2_TTF_PATH}" ] && echo "SDL2_ttf not found!" && exit 1
 
@@ -30,7 +30,7 @@ echo "SDL2_ttf found at ${SDL2_TTF_PATH}"
 [[ ! -d "${SDL2_MIXER_PATH}" && -d "/opt/homebrew/Cellar/sdl2_mixer" ]] &&
     export SDL2_MIXER_PATH=$(find /opt/homebrew/Cellar/sdl2_mixer -name "2.*" -type d | head -n 1)
 [[ ! -d "${SDL2_MIXER_PATH}" && -d "/tmp/.sdl/" ]] &&
-    export SDL2_MIXER_PATH=$(find /tmp/.sdl/ -name "SDL2_mixer-2.*" -type d | head -n 1)
+    export SDL2_MIXER_PATH=$(find /tmp/.sdl/ -name "SDL_mixer-*" -type d | head -n 1)
 
 [ ! -d "${SDL2_MIXER_PATH}" ] && echo "SDL2_mixer not found!" && exit 1
 
