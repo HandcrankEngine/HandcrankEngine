4ac8721764fc82aee66a61d250011f231df60f54
Scott Doxey <neogeek0101@gmail.com>
2025-07-26 07:52:43 -0400

    Moved audio load back into main header.
    Added back missing SFX methods.

--- include/Handcrank/Audio.hpp
+++ include/Handcrank/Audio.hpp
@@ -5,6 +5,8 @@
 
 #include <SDL_mixer.h>
 
+#include "AudioCache.hpp"
+
 namespace Handcrank
 {
 
@@ -26,6 +28,112 @@ inline auto PlaySFX(Mix_Chunk *sfx) -> int
     }
 
     return Mix_PlayChannel(-1, sfx, 0);
+}
+
+inline auto LoadMusic(const char *path) -> Mix_Music *
+{
+    if (audioMusicCache.find(path) != audioMusicCache.end())
+    {
+        return audioMusicCache.find(path)->second;
+    }
+
+    if (!audioLoadedForFirstTime)
+    {
+        SetupAudio();
+
+        audioLoadedForFirstTime = true;
+    }
+
+    auto *music = Mix_LoadMUS(path);
+
+    if (music == nullptr)
+    {
+        return nullptr;
+    }
+
+    audioMusicCache.insert_or_assign(path, music);
+
+    return music;
+}
+
+inline auto LoadMusic(const void *mem, const int size) -> Mix_Music *
+{
+    auto hash = MemHash(mem, size);
+
+    if (audioMusicCache.find(hash) != audioMusicCache.end())
+    {
+        return audioMusicCache.find(hash)->second;
+    }
+
+    if (!audioLoadedForFirstTime)
+    {
+        SetupAudio();
+
+        audioLoadedForFirstTime = true;
+    }
+
+    auto *rw = SDL_RWFromConstMem(mem, size);
+
+    if (rw == nullptr)
+    {
+        return nullptr;
+    }
+
+    auto *music = Mix_LoadMUS_RW(rw, 1);
+
+    if (music == nullptr)
+    {
+        return nullptr;
+    }
+
+    audioMusicCache.insert_or_assign(hash, music);
+
+    return music;
+}
+
+inline auto LoadSFX(const char *path) -> Mix_Chunk *
+{
+    if (audioSFXCache.find(path) != audioSFXCache.end())
+    {
+        return audioSFXCache.find(path)->second;
+    }
+
+    auto *sfx = Mix_LoadWAV(path);
+
+    if (sfx == nullptr)
+    {
+        return nullptr;
+    }
+
+    return sfx;
+}
+
+inline auto LoadSFX(const void *mem, const int size) -> Mix_Chunk *
+{
+    auto hash = MemHash(mem, size);
+
+    if (audioSFXCache.find(hash) != audioSFXCache.end())
+    {
+        return audioSFXCache.find(hash)->second;
+    }
+
+    auto *rw = SDL_RWFromConstMem(mem, size);
+
+    if (rw == nullptr)
+    {
+        return nullptr;
+    }
+
+    auto *sfx = Mix_LoadWAV_RW(rw, 1);
+
+    if (sfx == nullptr)
+    {
+        return nullptr;
+    }
+
+    audioSFXCache.insert_or_assign(hash, sfx);
+
+    return sfx;
 }
 
 } // namespace Handcrank
