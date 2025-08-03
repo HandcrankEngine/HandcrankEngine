74324c0a7c05376c7c0e3b5f87d4941375987bc0
Scott Doxey <neogeek0101@gmail.com>
2025-08-03 09:07:42 -0400

    Updated ImageRenderObject to use new TextureRenderObject.

--- include/Handcrank/ImageRenderObject.hpp
+++ include/Handcrank/ImageRenderObject.hpp
@@ -7,16 +7,14 @@
 #include <SDL_image.h>
 
 #include "Handcrank.hpp"
-#include "TextureCache.hpp"
+#include "TextureRenderObject.hpp"
 
 namespace Handcrank
 {
 
-class ImageRenderObject : public RenderObject
+class ImageRenderObject : public TextureRenderObject
 {
   protected:
-    SDL_Texture *texture;
-
     SDL_Rect srcRect = SDL_Rect();
 
     bool srcRectSet = false;
@@ -32,84 +30,12 @@ class ImageRenderObject : public RenderObject
   public:
     explicit ImageRenderObject() = default;
     explicit ImageRenderObject(const float x, const float y)
-        : RenderObject(x, y) {};
+        : TextureRenderObject(x, y) {};
     explicit ImageRenderObject(const float x, const float y, const float w,
                                const float h)
-        : RenderObject(x, y, w, h) {};
+        : TextureRenderObject(x, y, w, h) {};
 
     ~ImageRenderObject() override = default;
-
-    /**
-     * Set texture from an existing texture reference.
-     *
-     * @param texture A texture.
-     */
-    void SetTexture(SDL_Texture *texture)
-    {
-        this->texture = texture;
-
-        UpdateRectSizeFromTexture();
-    }
-
-    /**
-     * Load texture from a path.
-     *
-     * @param renderer A structure representing rendering state.
-     * @param path File path to texture file.
-     *
-     * @deprecated DEVELOPMENT USE ONLY! Use LoadTexture to load textures in a
-     * release build.
-     */
-    void LoadTexture(SDL_Renderer *renderer, const char *path)
-    {
-        texture = LoadCachedTexture(renderer, path);
-
-        UpdateRectSizeFromTexture();
-    }
-
-    /**
-     * Load texture from a read-only buffer.
-     *
-     * @param renderer A structure representing rendering state.
-     * @param mem A pointer to a read-only buffer.
-     * @param size The buffer size, in bytes.
-     */
-    void LoadTexture(SDL_Renderer *renderer, const void *mem, const int size)
-    {
-        texture = LoadCachedTexture(renderer, mem, size);
-
-        UpdateRectSizeFromTexture();
-    }
-
-    /**
-     * Load SVG texture from a string.
-     *
-     * @param renderer A structure representing rendering state.
-     * @param content Full SVG string including <svg></svg> tags.
-     */
-    void LoadSVGString(SDL_Renderer *renderer, const std::string &content)
-    {
-        texture = LoadCachedTexture(renderer, content.c_str(), content.size());
-
-        UpdateRectSizeFromTexture();
-    }
-
-    void UpdateRectSizeFromTexture()
-    {
-        if (texture == nullptr)
-        {
-            return;
-        }
-
-        int textureWidth;
-        int textureHeight;
-
-        SDL_QueryTexture(texture, nullptr, nullptr, &textureWidth,
-                         &textureHeight);
-
-        rect.w = textureWidth;
-        rect.h = textureHeight;
-    }
 
     void SetSrcRect(const SDL_Rect &srcRect)
     {
