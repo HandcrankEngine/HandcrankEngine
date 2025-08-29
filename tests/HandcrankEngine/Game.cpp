944f1069772b56f7045a3c4fd9a80925c88e5718
Scott Doxey <neogeek0101@gmail.com>
2025-08-29 17:27:30 -0400

    Added new test file for Game object.

---
+++ tests/HandcrankEngine/Game.cpp
@@ -0,0 +1,50 @@
+#include <cassert>
+#include <iostream>
+
+#include "HandcrankEngine/HandcrankEngine.hpp"
+
+using namespace Handcrank;
+
+auto main(int argc, char *argv[]) -> int
+{
+
+    {
+        auto *game = new Game();
+
+        std::cout << ".";
+    }
+
+    {
+        auto *game = new Game();
+
+        assert(size(game->children) == 0);
+
+        auto renderObject = std::make_shared<RenderObject>();
+
+        game->AddChildObject(renderObject);
+
+        assert(size(game->children) == 1);
+
+        std::cout << ".";
+    }
+
+    {
+        auto *game = new Game();
+
+        auto renderObject = std::make_shared<RenderObject>();
+
+        game->AddChildObject(renderObject);
+
+        assert(size(game->children) == 1);
+
+        renderObject->Destroy();
+
+        game->DestroyChildObjects();
+
+        assert(size(game->children) == 0);
+
+        std::cout << ".";
+    }
+
+    return 0;
+}
