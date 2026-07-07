1ce53409bc9dc9b86f7a32878bd4fde5638deb87
Scott Doxey <neogeek0101@gmail.com>
2026-07-07 01:32:31 -0400

    Removed unused flag from build command in bash script.

--- bin/build-web.sh
+++ bin/build-web.sh
@@ -5,7 +5,7 @@
     mkdir -p /tmp/dist/web
 
     emcc -std=c++17 -Os -o /tmp/dist/web/index.html src/*.cpp -Ifonts -Iimages -Iinclude -Iexamples -Isrc \
-        -I"${DEPS_PREFIX}"/include/SDL2 -I"${DEPS_PREFIX}"/include -L"${DEPS_PREFIX}"/lib \
+        -I"${DEPS_PREFIX}"/include -L"${DEPS_PREFIX}"/lib \
         -lSDL3 -lSDL3_image -lSDL3_ttf -lSDL3_mixer \
         -s USE_FREETYPE=1 -s USE_HARFBUZZ=1 \
         -s MAX_WEBGL_VERSION=2 \
